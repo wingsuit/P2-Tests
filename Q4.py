eb6a4ffbbd2e7e7999b59133470deb1bd521ddc7
wingsuit <wingsuit@riseup.net>
2019-10-18 00:02:14 +1100

    New Tests

--- Q4.py
+++ Q4.py
@@ -13,46 +13,62 @@ def test():
             {'size': 4,'entrance': (0, 0),'exit': (2, 1),'dragon': (0, 2),'sword': (3, 3),'treasure': [(1, 3)],'walls': [(1, 1), (1, 2), (2, 2), (2, 3)]},
             "23",
             "Example 1"
-        ),
-        (
+        ),(
             {'size': 5,'entrance': (0, 2),'exit': (0, 4),'sword': (0, 0),'dragon': (2, 1),'treasure': [(0, 3), (4, 1)],'walls': [(1, 0), (2, 0), (3, 0), (1, 2), (2, 2), (3, 2), (1, 3), (2, 3), (3, 3)]},
             "14",
             "Example 2"
-        ),
-        (
+        ),(
             {'size': 5,'entrance': (0, 2),'exit': (4, 4),'sword': (0, 0),'dragon': (2, 1),'treasure': [(0, 3), (4, 1)],'walls': [(4,3),(3,4),(1, 0), (2, 0), (3, 0), (1, 2), (2, 2), (3, 2), (1, 3), (2, 3), (3, 3)]},
             'None',
             "Exit unreachable"
-        ),
-        (
+        ),(
             {'size': 5,'entrance': (0, 2),'exit': (0, 4),'dragon': (2, 1),'treasure': [(0, 3), (4, 1)],'walls': [(1, 0), (2, 0), (3, 0), (1, 2), (2, 2), (3, 2), (1, 3), (2, 3), (3, 3)]},
             '16',
             "No sword"
-        ),
-        (
+        ),(
             {'size': 6, 'entrance': (0, 1), 'exit': (0,5), 'dragon': (0, 3), 'sword': (0, 0)},
             '6',
             "No treasure or walls. Grab sword kill dragon exit"
-        ),
-        (
+        ),(
             {'size': 6, 'entrance': (0, 1), 'exit': (0,5), 'dragon': (0, 3)},
             '8',
             "No treasure, walls or sword. Walk around dragon"
-        ),
-        (
+        ),(
             {'size': 6, 'entrance': (5, 5), 'exit': (0,1)},
             '9',
             "Just entrance and exit, far"
-        ),
-        (
+        ),(
             {'size': 6, 'entrance': (0, 0), 'exit': (0,1)},
             '1',
             "Just entrance and exit, close"
-        ),
-        (
+        ),(
             {'size': 6, 'entrance': (0, 0), 'exit': (2, 2), 'walls': [(0, 1), (1, 1), (1, 0)]},
             'None',
             "Just entrance and exit, but exit unreachable"
+        ),(
+            {'size': 5,'entrance': (0, 2),'exit': (0, 4),'sword': (0, 0),'dragon': (2, 1),'treasure': [(0, 3), (3, 4)],'walls': [(1, 0), (2, 0), (3, 0), (1, 2), (2, 2), (3, 2), (1, 3), (2, 3), (3, 3)]},
+            '8',
+            "Quicker without the sword"
+        ),(
+            {'size': 7,'entrance': (0, 0),'exit': (6, 6),'dragon': (0, 2),'sword': (3, 3),'treasure': [(1, 3)],'walls': [(1, 1), (1, 2), (2, 2), (2, 3)]},
+            "18",
+            "Big"
+        ),(
+            {'size': 8,'entrance': (0, 0),'exit': (7, 7),'dragon': (0, 2),'sword': (3, 3),'treasure': [(1, 3)],'walls': [(1, 1), (1, 2), (2, 2), (2, 3)]},
+            "20",
+            "Bigger"
+        ),(
+            {'size': 9,'entrance': (0, 0),'exit': (8, 8),'dragon': (0, 2),'sword': (3, 3),'treasure': [(1, 3)],'walls': [(1, 1), (1, 2), (2, 2), (2, 3)]},
+            "22",
+            "Biggerrr"
+        ),(
+            {'size': 10,'entrance': (0, 0),'exit': (9, 9),'dragon': (0, 2),'sword': (3, 3),'treasure': [(1, 3)],'walls': [(1, 1), (1, 2), (2, 2), (2, 3)]},
+            "24",
+            "Biggest"
+        ),(
+            {'size': 11,'entrance': (0, 0),'exit': (10, 10),'dragon': (0, 2),'sword': (3, 3),'treasure': [(1, 3)],'walls': [(1, 1), (1, 2), (2, 2), (2, 3)]},
+            "26",
+            "Biggest"
         )
     ]
     
